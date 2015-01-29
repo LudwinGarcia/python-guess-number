96da31c6d5d23e67c70f7dd59a5f853ee148e4cb
Ludwin Garcia <lgarcia@cognits.co>
2015-01-29 10:53:34 -0600

    Added documentation and change some 'prints'

--- application.py
+++ application.py
@@ -7,9 +7,7 @@ analyze and restructure, if you find a better way
 applying an area in the code, you can comment it on Github Issues.
 """
 
-import random
-import sys
-
+import random # Module to generate random number
 
 instructions = """
 ################################################################
@@ -30,48 +28,67 @@ instructions = """
 
 countinue_all_game = True # Controls if the application run again.
 
-print instructions # Display instructions
+print instructions # Display instructions.
 
 while countinue_all_game == True:
     random_number = random.randint(1, 20)
     print "++++++ %s ++++++" % random_number
-    turn = 1 # Controls opportunities to guess number
-    countinue_turn = True
-    aceptChoise = False
+    turn = 1 # Controls opportunities to guess number.
+    countinue_turn = True # Controls if the turn continues.
+    aceptChoise = False # Controls if the user want play again.
 
+    # This "while" allows continue the turn.
     while countinue_turn == True:
         # This "try" controls the errors that occurred in the application.
         try:
+            # Controls number of turn
             if turn <= 4:
                 print "\n>>>> Your number turn is: %s <<<<" % turn
                 user_input = raw_input("\n    * Please enter a number: ")
+
+                # Verify if the input contains only numbers.
                 if user_input.isdigit() == True:
                     user_input = int(user_input)
-                    # Verify if input is one between twenty 
+
+                    # Verify if input is one between twenty.
                     if user_input > 0 and user_input <= 20:
+
+                            # Verify if input is equal to the guess number or more low or more hight.
                             if user_input == random_number:
-                                print "    * You Win!!!"
+                                print "\n\n    ((( ^.^ You Win!!! ^.^ )))"
                                 print "\n\n Want to play again?"
+
+                                # This "while" allows continue if the choice is incorrect.
                                 while aceptChoise == False:
                                     try:
                                         choise = raw_input("\n\n please enter \"Y\" to play again or enter \"N\" to Exit: ")
                                         choise = choise.lower()
+
                                         if choise == "y":
-                                            countinue_turn = False
-                                            aceptChoise = True
+                                            print "\n\n"
+                                            print "+++++++++++++++++++++++"
+                                            print "++++++ New Game! ++++++"
+                                            print "+++++++++++++++++++++++"
+                                            countinue_turn = False #Stops the turn.
+                                            aceptChoise = True # When the choice is correct.
                                         elif choise == "n":
-                                            print "\n See you soon!"
-                                            countinue_all_game = False
-                                            countinue_turn = False
-                                            aceptChoise = True
+                                            print "\n   ((((   See you soon! XD  )))"
+                                            countinue_turn = False # Stops the turn.
+                                            countinue_all_game = False # Stops the game.
+                                            aceptChoise = True # When the choice is correct.
                                         else:
                                             print "Your choice does not exist..."
-                                            pass
+
+                                    # Capture a bug in the play again
                                     except ValueError as error:
                                         print "Error ocurred: %s" % error
+
+                            # Verify if user_input is more high
                             elif user_input > random_number:
                                 print "    * You guessed too high, please try again"
                                 turn += 1
+
+                            # Verify if user_input is more low
                             elif user_input < random_number:
                                 print "    * You guessed too low, please try again"
                                 turn += 1
@@ -79,30 +96,38 @@ while countinue_all_game == True:
                         print "    * You only can enter numbers the one to twenty"
                         turn += 1
                 else:
-                    print """
+                    print """\n
                     \r    * You can not enter letters, special caracters or decimal numbers; 
                     \r      Please enter a integer number"""
                     turn += 1
-            else:
-                print "\n\n Game Over!"
-                print random_number
+            else: # "else" if the user fail.
+                print "\n\n    ###  Game Over!!! :C  ###"
+                print "\t===>  The number to guess is %s  <===" % random_number
                 print "\n\n Want to play again?"
                 while aceptChoise == False:
                     try:
                         choise = raw_input("\n\n please enter \"Y\" to play again or enter \"N\" to Exit: ")
                         choise = choise.lower()
+
                         if choise == "y":
-                            countinue_turn = False
-                            aceptChoise = True
+                            print "\n\n"
+                            print "+++++++++++++++++++++++"
+                            print "++++++ New Game! ++++++"
+                            print "+++++++++++++++++++++++"
+                            countinue_turn = False # Stops the turn.
+                            aceptChoise = True # When the choice is correct.
                         elif choise == "n":
-                            print "\n See you soon!"
-                            countinue_all_game = False
-                            countinue_turn = False
-                            aceptChoise = True
+                            print "\n   ((((   See you soon! XD  )))"
+                            countinue_turn = False # Stops the turn.
+                            countinue_all_game = False # Stops the game
+                            aceptChoise = True # When the choice is correct
                         else:
                             print "Your choice does not exist..."
-                            pass
+
+                    # Capture a bug in the play again
                     except ValueError as error:
                         print "Error ocurred: %s" % error
+
+        # Capture a bug in the game
         except ValueError as error:
             print " Error ocurred: %s, plese try again" % error
